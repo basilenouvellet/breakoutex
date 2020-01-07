d4e165748a19eb80be57477a75348393aca9d520
Basile NOUVELLET <basile.nouvellet@ponts.org>
2020-01-07 17:21:26 +0100

    Mix Phoenix base

---
+++ lib/breakoutex_web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule BreakoutexWeb.PageController do
+  use BreakoutexWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+end
---
+++ lib/breakoutex_web/router.ex
@@ -0,0 +1,26 @@
+defmodule BreakoutexWeb.Router do
+  use BreakoutexWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", BreakoutexWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", BreakoutexWeb do
+  #   pipe_through :api
+  # end
+end
