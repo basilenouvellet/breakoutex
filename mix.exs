98a6161f35e01c9fd5dfbae2068b0d86315b526c
Basile NOUVELLET <basile.nouvellet@ponts.org>
2020-01-07 19:00:13 +0100

    Add blocks rendering logic

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule Breakoutex.MixProject do
       {:phoenix_live_view, "~> 0.3.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:elixir_uuid, "~> 1.2"}
     ]
   end
 end
